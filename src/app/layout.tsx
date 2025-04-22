770b7a294f710f02068a702cd03ff22b7e240fa2
tmdgns1126 <tmdgns1126@naver.com>
2025-04-22 15:48:03 +0900

    Feat: favicon, og-image 추가 및 layout 설정 #5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,48 @@
-import { Metadata } from "next";
-import "./globals.css";
+import { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "훈스에씨오 for SEO",
-  description: "SEO 연구 프로젝트",
+	title: '훈세씨오',
+	description: 'SEO 연구 프로젝트',
+	icons: {
+		icon: '/favicon.png',
+	},
+	openGraph: {
+		title: '훈세씨오',
+		description: 'SEO 연구 프로젝트',
+		url: 'https://seo.huns.site',
+		siteName: 'huns-seo',
+		images: [
+			{
+				url: 'https://seo.huns.site/og-image.png',
+				width: 1200,
+				height: 630,
+			},
+		],
+		locale: 'ko_KR',
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: '훈세씨오',
+		description: 'SEO 연구 프로젝트',
+		creator: '@orchemi',
+		images: ['https://seo.huns.site/og-image.png'],
+	},
+	viewport: {
+		width: 'device-width',
+		initialScale: 1,
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ko">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ko">
+			<body>{children}</body>
+		</html>
+	);
 }
