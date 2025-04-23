b5c2048034f628a16a9ede950eb592e9258c7707
tmdgns1126 <tmdgns1126@naver.com>
2025-04-23 16:10:06 +0900

    Remove: 불필요 Google Script 파일 제거 #20

--- src/components/google-scripts.tsx
+++
@@ -1,8 +0,0 @@
-import { GOOGLE } from '@/config/google.config';
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
-
-const GoogleScripts = () => {
-	return <></>;
-};
-
-export default GoogleScripts;
